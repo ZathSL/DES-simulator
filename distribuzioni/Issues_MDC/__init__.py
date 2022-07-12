ce19b902185c38ea404f1a94ef59514dc8c2b88e
Alessandro Contenti <a.contenti@gmail.com>
2022-07-12 13:34:38 +0200

    - added Strutture distributions - improved MDC distributions
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- distribuzioni/Issues_MDC/__init__.py
+++ distribuzioni/Issues_MDC/__init__.py
@@ -8,40 +8,38 @@ matplotlib.style.use('ggplot')
 
 def get_dataset():
     df = pd.read_csv("../../dataset/Dataset_SDO_Regione_Lombardia.csv", dtype=str)
-    df = df[["CODICE MDC", "DESCRIZIONE MDC"]].astype("string").fillna("NA")
+    df = df[["CODICE MDC", "DESCRIZIONE MDC"]]
+    missing = df["CODICE MDC"].isna()
+    df.loc[missing, "CODICE MDC"] = df.loc[missing, "DESCRIZIONE MDC"].map({
+        "MDC NON APPLICABILE": "NA",
+        "PREMDC (DRG SENZA SPECIFICA MDC)": "PR",
+    })
     return df
 
 
 def plot_data(data: pd.DataFrame):
-    data = data.value_counts(sort=False).to_frame("count").reset_index(level="DESCRIZIONE MDC").drop(
-        "#").sort_index().reset_index()
-    data = data.groupby(["DESCRIZIONE MDC"], as_index=False).agg({
-        "CODICE MDC": "last",
-        "DESCRIZIONE MDC": "last",
-        "count": "sum",
-    })
-    data = data.set_index("CODICE MDC").sort_index()
-    data.to_csv("MDCDistribution.csv")
-    codes = data["count"]
+    data = data.value_counts(sort=False, dropna=False).to_frame("CONTEGGIO")
+    data = data.reset_index(level="DESCRIZIONE MDC").drop("#").sort_index()
+    freq = data["FREQUENZA"] = data["CONTEGGIO"] / data["CONTEGGIO"].sum()
     # Display
     fig, ax = plt.subplots(1, 1)
-    codes.plot(kind='bar', ax=ax)
-    ax.set_title('MDC distribution')
-    ax.set_xlabel('MDC')
-    ax.set_ylabel('Frequency')
+    freq.plot(kind='bar', ax=ax)
+    ax.set_title("Distribuzione degli MDC")
+    ax.set_xlabel("MDC")
+    ax.set_ylabel("Frequenza")
     plt.savefig('MDCDistribution.jpg')
 
     # Display2
     fig, ax = plt.subplots(1, 1)
     ax.set_yscale('log')
-    codes.plot(kind='bar', ax=ax)
-    ax.set_title('MDC distribution')
-    ax.set_xlabel('MDC')
-    ax.set_ylabel('Frequency')
+    freq.plot(kind='bar', ax=ax)
+    ax.set_title("Distribuzione degli MDC")
+    ax.set_xlabel("MDC")
+    ax.set_ylabel("Frequenza")
     plt.savefig('MDCDistributionLOG.jpg')
 
     # Export CSV
-    data.to_csv("MDCDistribution.csv")
+    data.to_csv("MDCDistribution.csv", float_format="%.15f", encoding="utf-8")
 
 
 def main():
---
+++ distribuzioni/Strutture/__init__.py
@@ -0,0 +1,69 @@
+import os
+from os import path
+
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+
+matplotlib.rcParams['figure.figsize'] = (50, 20)
+matplotlib.style.use('ggplot')
+cwd = path.dirname(path.abspath(__file__))
+
+
+def get_dataset(*columns: str):
+    df = pd.read_csv("../../dataset/Dataset_SDO_Regione_Lombardia.csv", dtype=str)
+    df = df[["CODICE MDC", "DESCRIZIONE MDC", *columns]]
+    missing = df["CODICE MDC"].isna()
+    df.loc[missing, "CODICE MDC"] = df.loc[missing, "DESCRIZIONE MDC"].map({
+        "MDC NON APPLICABILE": "NA",
+        "PREMDC (DRG SENZA SPECIFICA MDC)": "PR",
+    })
+    mdcs = df[["CODICE MDC", "DESCRIZIONE MDC"]].value_counts(sort=False).to_frame("count") \
+        .reset_index(level="DESCRIZIONE MDC").drop("#").sort_index().reset_index().drop("count", axis=1)
+    return df, mdcs
+
+
+def plot_data(data: pd.DataFrame, mdc: str, descrizione_mdc: str):
+    data = data.value_counts(sort=False).to_frame("CONTEGGIO")
+    data = data.reset_index()
+    data = data.groupby(["CODICE STRUTTURA DI RICOVERO"], as_index=False).agg({
+        "CODICE STRUTTURA DI RICOVERO": "last",
+        "DESCRIZIONE STRUTTURA DI RICOVERO": "last",
+        "CONTEGGIO": "sum",
+    })
+    data = data.set_index("CODICE STRUTTURA DI RICOVERO").sort_index()
+    freq = data["FREQUENZA"] = data["CONTEGGIO"] / data["CONTEGGIO"].sum()
+
+    os.makedirs(f"MDC_{mdc}", exist_ok=True)
+    # Display
+    fig, ax = plt.subplots(1, 1)
+    freq.plot(kind='bar', ax=ax)
+    ax.set_title(f"Distribuzione tra le strutture dell\'MDC {mdc} ({descrizione_mdc})")
+    ax.set_xlabel("CODICE STRUTTURA DI RICOVERO")
+    ax.set_ylabel("Frequenza")
+    plt.savefig(path.join(cwd, f"MDC_{mdc}", "StruttureDistribution.jpg"))
+
+    # Display2
+    fig, ax = plt.subplots(1, 1)
+    ax.set_yscale('log')
+    freq.plot(kind='bar', ax=ax)
+    ax.set_title(f"Distribuzione tra le strutture dell\'MDC {mdc} ({descrizione_mdc})")
+    ax.set_xlabel("CODICE STRUTTURA DI RICOVERO")
+    ax.set_ylabel("Frequenza")
+    plt.savefig(path.join(cwd, f"MDC_{mdc}", "StruttureDistributionLOG.jpg"))
+
+    # Export CSV
+    data.to_csv(path.join(cwd, f"MDC_{mdc}", "StruttureDistribution.csv"), float_format="%.15f", encoding="utf-8")
+
+
+def main():
+    df, mdcs = get_dataset("CODICE STRUTTURA DI RICOVERO", "DESCRIZIONE STRUTTURA DI RICOVERO")
+    for i, mdc in mdcs.iterrows():
+        codice, descrizione = mdc["CODICE MDC"], mdc["DESCRIZIONE MDC"]
+        print(i + 1, codice, descrizione)
+        df_mdc = df[df["CODICE MDC"] == codice].drop(["CODICE MDC", "DESCRIZIONE MDC"], axis=1)
+        plot_data(df_mdc, codice, descrizione)
+
+
+if __name__ == '__main__':
+    main()
