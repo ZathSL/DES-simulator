9e784782865f76be0cc0d91d7269e20aec35e21d
Alessandro Contenti <a.contenti@gmail.com>
2022-07-13 11:50:35 +0200

    - added daily average to MDC distributions
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- distribuzioni/MDC/__init__.py
+++ distribuzioni/MDC/__init__.py
@@ -26,8 +26,9 @@ def plot_data(data: pd.DataFrame):
         "CONTEGGIO": "mean",
     })
     data = data.set_index("CODICE MDC").drop(columns="ANNO").drop("#").sort_index()
-    freq = data["FREQUENZA"] = data["CONTEGGIO"] / data["CONTEGGIO"].sum()
     data.rename(columns={"CONTEGGIO": "CONTEGGIO PER ANNO"}, inplace=True)
+    data["CONTEGGIO PER GIORNO"] = data["CONTEGGIO PER ANNO"] / 365.0
+    freq = data["FREQUENZA"] = data["CONTEGGIO PER ANNO"] / data["CONTEGGIO PER ANNO"].sum()
     # Display
     fig, ax = plt.subplots(1, 1)
     freq.plot(kind='bar', ax=ax)
