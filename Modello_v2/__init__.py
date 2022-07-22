0e70589b4e5bd38bbb432d6e4f6ff82a04a93e89
zathSL <simone.lissandrello@edu.unito.it>
2022-07-22 17:26:34 +0200

    bug fix

--- Modello_v2/__init__.py
+++ Modello_v2/__init__.py
@@ -1,9 +1,8 @@
 import salabim as sim
 import scipy.stats.distributions as st
-
+import math
 
 gennorm = iter(sim.External(st.gennorm.rvs(beta=0.16, size=1000, loc=1, scale=0)).dis)
-print(next(gennorm))
 # norminvgauss = iter(sim.External(st.norminvgauss.rvs(a=4.06, b=4.06, size=1000, loc=1.51, scale=1.76)).dis)
 kappa3_1 = iter(sim.External(st.kappa3.rvs(a=1.99, size=1000, loc=1, scale=9.82)).dis)
 kappa3_2 = iter(sim.External(st.kappa3.rvs(a=3.14, size=1000, loc=1, scale=7.68)).dis)
@@ -15,34 +14,44 @@ foldnorm = iter(sim.External(st.foldnorm.rvs(c=0.03, size=1000, loc=0, scale=5.5
 class Patient(sim.Component):
     def setup(self, mdc, structure, *args, **kwargs):
         self.mdc = mdc
-        self.enqueue = False
+        self.visited_already = False
         self.dh = 0
         self.do = 0
         self.ds = 0
         self.days_do = 0
+        self.structure = structure
         structure.entry_patient(self)
 
     def assign(self, ds, dh, do, days_do):
-        self.enqueue = True
-        self.ds = ds
-        self.dh = dh
-        self.do = do
-        self.days_do = days_do
+        self.visited_already = True
+        self.ds = math.ceil(ds)
+        self.dh = math.ceil(dh)
+        self.do = math.ceil(do)
+        self.days_do = math.ceil(days_do)
+        print(ds, dh, do, days_do)
+        print(self.ds, self.dh, self.do, self.days_do)
 
     def dec(self, dec_ds, dec_dh, dec_do):
         self.ds = self.ds - dec_ds
         self.dh = self.dh - dec_dh
         self.do = self.do - dec_do
+        print(self.ds, self.dh, self.do)
 
     def hospitalization(self, unit):
-        yield self.hold(unit)
+        #yield self.wait(unit)
+        if self.do == 0 and self.ds == 0 and self.dh == 0:
+            self.structure.list_patients.append(self)
+        else:
+            self.structure.visit_waiting.append(self)
 
 
 class Structure(sim.Component):
     def setup(self):
         self.acceptance_waiting = sim.Queue("acceptance")
         self.visit_waiting = sim.Queue("visit")
-        self.hospitalization_waiting = sim.Queue("hospitalization")
+        self.hospitalization_waitingDS = sim.Queue("hospitalizationDS")
+        self.hospitalization_waitingDO = sim.Queue("hospitalizationDO")
+        self.hospitalization_waitingDH = sim.Queue("hospitalizationDH")
         self.worktodo = sim.State('worktodo')
         self.list_patients = []
 
@@ -50,7 +59,7 @@ class Structure(sim.Component):
         patient.enter(self.acceptance_waiting)
 
     def visit_patient(self, patient):
-        if not patient.enqueue:
+        if not patient.visited_already:
             if 1 <= patient.mdc <= 13 or 15 <= patient.mdc <= 20:
                 do = next(gennorm)
             else:
@@ -63,39 +72,18 @@ class Structure(sim.Component):
             ds = abs(next(foldnorm))
             dh = abs(next(genhalf))
             patient.assign(ds, dh, do, days_do)
-        self.hospitalization_waiting.append(patient)
-
-    def recovery_patient(self, patient):
-        if (patient.do and patient.ds and patient.dh) == 0:
-            self.list_patients.append(patient)
-        else:
-            unit = 0
-            if patient.ds > 0:
-                unit = unit + 1
-                if patient.do > 0:
-                    unit = unit + patient.days_do
-                    patient.hospitalization(unit)
-                    patient.dec(dec_ds=1, dec_dh=0, dec_do=1)
-                else:
-                    patient.hospitalization(unit)
-                    patient.dec(dec_ds=1, dec_dh=0, dec_do=0)
-            elif patient.dh > 0:
-                unit = unit + 1
-                if patient.dh > 0:
-                    unit = unit + patient.days_do
-                    patient.hospitalization(unit)
-                    patient.dec(dec_ds=0, dec_dh=1, dec_do=1)
-                else:
-                    patient.hospitalization(unit)
-                    patient.dec(dec_ds=0, dec_dh=1, dec_do=0)
-            elif patient.do > 0:
-                unit = patient.days_do
-                patient.hospitalization(unit)
-                patient.dec(dec_ds=0, dec_dh=0, dec_do=1)
+        if patient.ds > 0:
+            self.hospitalization_waitingDS.append(patient)
+        elif patient.dh > 0:
+            self.hospitalization_waitingDH.append(patient)
+        elif patient.do > 0:
+            self.hospitalization_waitingDO.append(patient)
 
     def running_process(self):
         while True:
-            if len(self.acceptance_waiting) == 0 and len(self.visit_waiting) == 0 and len(self.hospitalization_waiting) == 0:
+            if len(self.acceptance_waiting) == 0 and len(self.visit_waiting) == 0 and \
+                    len(self.hospitalization_waitingDS) == 0 and len(self.hospitalization_waitingDH) == 0 and \
+                    len(self.hospitalization_waitingDO) == 0:
                 yield self.wait((self.worktodo, True, 1))
             if len(self.acceptance_waiting) > 0:
                 # accepted patient
@@ -103,8 +91,18 @@ class Structure(sim.Component):
             if len(self.visit_waiting) > 0:
                 # patient visit
                 self.visit_patient(self.visit_waiting.pop())
-            if len(self.hospitalization_waiting) > 0:
-                self.recovery_patient(self.hospitalization_waiting.pop())
+            if len(self.hospitalization_waitingDS) > 0:
+                temp_patient = self.hospitalization_waitingDS.pop()
+                temp_patient.dec(dec_ds=1, dec_dh=0, dec_do=0)
+                temp_patient.hospitalization(1)
+            if len(self.hospitalization_waitingDH) > 0:
+                temp_patient = self.hospitalization_waitingDH.pop()
+                temp_patient.dec(dec_ds=0, dec_dh=1, dec_do=0)
+                temp_patient.hospitalization(1)
+            if len(self.hospitalization_waitingDO) > 0:
+                temp_patient = self.hospitalization_waitingDO.pop()
+                temp_patient.dec(dec_ds=0, dec_dh=0, dec_do=1)
+                temp_patient.hospitalization(temp_patient.days_do)
 
 
 env = sim.Environment(trace=True)
@@ -113,6 +111,8 @@ patient = Patient(mdc=10, structure=structure)
 structure.activate(process='running_process')
 env.run(till=365)
 print()
-structure.hospitalization_waiting.print_statistics()
+structure.hospitalization_waitingDS.print_statistics()
+structure.hospitalization_waitingDH.print_statistics()
+structure.hospitalization_waitingDO.print_statistics()
 structure.acceptance_waiting.print_statistics()
 structure.visit_waiting.print_statistics()
