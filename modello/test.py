feffee89edebf7c91c335b3cfbdf21260a52223f
Alessandro Contenti <a.contenti@gmail.com>
2022-07-30 16:55:43 +0200

    - refactored tests - created single modular test function
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- modello/test.py
+++ modello/test.py
@@ -1,5 +1,3 @@
-import inspect
-
 import numpy as np
 import pandas as pd
 
@@ -7,163 +5,81 @@ from model import simulation, Mutation
 from util import CodeTimer
 
 
-def test_main():
-    logfile = False  # open("sim_trace.log", "w")
-    sim_time_days = 365
-    animate = False
-    speed = 10
-    mutations = []
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
-
-
 def test_mutations_example():
-    logfile = False  # open("sim_trace.log", "w")
-    sim_time_days = 365
-    animate = False
-    speed = 10
-    mutations = [  # mutazioni di esempio, l'ordine delle mutazioni conta
+    return [  # mutazioni di esempio, l'ordine delle mutazioni conta
         Mutation(type="structure", id="030070-00", ops={"delete": True}),  # elimina la struttura
         Mutation(type="structure", id="030017-00", ops={"beds": 10}),  # imposta i letti della struttura a 10
         Mutation(type="structure", id="030038-00", ops={"beds": 0.50}),  # riduce i letti della struttura del 50%
         Mutation(type="structure", id="*", ops={"beds": 1.10}),  # aumenta i letti di tutte le strutture del 10%
     ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
 
 
 def test_delete_5_biggest_structures():
-    logfile = False
-    sim_time_days = 365
-    animate = False
-    speed = 15
-    mutations = [
+    return [
         Mutation(type="structure", id="030901-01", ops={"delete": True}),
         Mutation(type="structure", id="030905-00", ops={"delete": True}),
         Mutation(type="structure", id="030906-00", ops={"delete": True}),
         Mutation(type="structure", id="030913-00", ops={"delete": True}),
         Mutation(type="structure", id="030924-00", ops={"delete": True}),
     ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
 
 
 def test_delete_5_smallest_structures():
-    logfile = False
-    sim_time_days = 365
-    animate = False
-    speed = 10
-    mutations = [
+    return [
         Mutation(type="structure", id="030070-00", ops={"delete": True}),
         Mutation(type="structure", id="030071-00", ops={"delete": True}),
         Mutation(type="structure", id="030071-01", ops={"delete": True}),
         Mutation(type="structure", id="030075-00", ops={"delete": True}),
         Mutation(type="structure", id="030079-00", ops={"delete": True})
     ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
 
 
-def test_decrease_all_beds_10():
-    logfile = False
-    sim_time_days = 365
-    animate = False
-    speed = 15
-    mutations = [
-        Mutation(type="structure", id="*", ops={"beds": 0.90})
+def test_decrease_all_beds_percent(percent: float):
+    return [
+        Mutation(type="structure", id="*", ops={"beds": 1.0 - percent / 100}),
     ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
 
 
-def test_increase_all_beds_10():
-    logfile = False
-    sim_time_days = 365
-    animate = False
-    speed = 15
-    mutations = [
-        Mutation(type="structure", id="*", ops={"beds": 1.10})
+def test_increase_all_beds_percent(percent: float):
+    return [
+        Mutation(type="structure", id="*", ops={"beds": 1.0 + percent / 100}),
     ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
 
 
-def test_decrease_all_beds_5():
-    logfile = False
-    sim_time_days = 365
-    animate = False
-    speed = 15
-    mutations = [
-        Mutation(type="structure", id="*", ops={"beds": 0.95})
+def test_change_convalescence_avg_time(time: int):
+    return [
+        Mutation(type="parameter", id="convalescence_avg_time", ops={"value": time}),
     ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
 
 
-def test_increase_all_beds_5():
-    logfile = False
-    sim_time_days = 365
-    animate = False
-    speed = 15
-    mutations = [
-        Mutation(type="structure", id="*", ops={"beds": 1.05})
-    ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
-
-
-def test_change_convalescence_avg_time():
-    logfile = False
-    sim_time_days = 365
-    animate = False
-    speed = 15
-    mutations = [
-        Mutation(type="parameter", id="convalescence_avg_time", ops={"value": 15}),
-    ]
-    simulation(trace=logfile, sim_time_days=sim_time_days, animate=animate, speed=speed, mutations=mutations,
-               statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + "/")
-
-
-def test_mdc_distributions():
-    sim_time_days = 30
-    mdc_stats = []
-    for i in range(10):
-        simulation(trace=False, sim_time_days=sim_time_days, animate=False, speed=10, mutations=[],
-                   statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + f"/runs/{i}/",
-                   random_seed=i)
-        csv = pd.read_csv(
-            "../statistiche/" + inspect.currentframe().f_code.co_name + f"/runs/{i}/number_patient_mdc.csv",
-            keep_default_na=False, dtype={"MDC": str}, index_col="MDC")
-        mdc_stats.append(csv)
-    frame = pd.concat((df["FREQUENCY"].rename(str(i)) for i, df in enumerate(mdc_stats)), axis=1)
+def calc_mdc_distributions(name: str, runs: int):
+    stats = []
+    for i in range(runs):
+        csv = pd.read_csv(f"../statistiche/{name}/runs/{i}/number_patient_mdc.csv", keep_default_na=False,
+                          dtype={"MDC": str}, index_col="MDC")
+        stats.append(csv)
+    frame = pd.concat((df["FREQUENCY"].rename(str(i)) for i, df in enumerate(stats)), axis=1)
     mean = frame.transpose().mean()
     mean /= mean.sum()
     mean.rename("FREQUENCY", inplace=True)
-    mean.to_csv("../statistiche/" + inspect.currentframe().f_code.co_name + "/number_patient_mdc_mean.csv",
-                float_format="%.15f", encoding="utf-8")
+    mean.to_csv(f"../statistiche/{name}/number_patient_mdc_mean.csv", float_format="%.15f", encoding="utf-8")
     original = pd.read_csv("../distribuzioni/empiriche/MDC/MDCDistribution.csv", keep_default_na=False,
                            dtype={"CODICE MDC": str}, index_col="CODICE MDC")
     original = original["FREQUENZA"]
     pearson = mean.corr(original, method="pearson")
     spearman = mean.corr(original, method="spearman")
     kendall = mean.corr(original, method="kendall")
-    with open("../statistiche/" + inspect.currentframe().f_code.co_name + "/correlation.txt", "w") as f:
+    with open(f"../statistiche/{name}/correlation.txt", "w") as f:
         f.write("Pearson: " + str(pearson) + "\n")
         f.write("Spearman: " + str(spearman) + "\n")
         f.write("Kendall: " + str(kendall) + "\n")
 
 
-def test_hospitalization_type_distributions():
-    runs = 2
-    sim_time_days = 5
+def calc_hospitalization_type_distributions(name: str, runs: int):
     stats = []
     for i in range(runs):
-        simulation(trace=False, sim_time_days=sim_time_days, animate=False, speed=10, mutations=[],
-                   statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + f"/runs/{i}/",
-                   random_seed=i)
-        csv = pd.read_csv(
-            "../statistiche/" + inspect.currentframe().f_code.co_name + f"/runs/{i}/type_patients_treated.csv",
-            keep_default_na=False, index_col="STRUTTURA")
+        csv = pd.read_csv(f"../statistiche/{name}/runs/{i}/type_patients_treated.csv", keep_default_na=False,
+                          index_col="STRUTTURA")
         stats.append(csv)
     dfs = []
     for key in stats[0].columns:
@@ -177,11 +93,10 @@ def test_hospitalization_type_distributions():
         dfs.append(df)
     concat = pd.concat(dfs, axis=1)
     concat.sort_index(inplace=True)
-    concat.to_csv("../statistiche/" + inspect.currentframe().f_code.co_name + "/type_patients_treated_mean.csv",
-                  float_format="%.15f", encoding="utf-8")
+    concat.to_csv(f"../statistiche/{name}/type_patients_treated_mean.csv", float_format="%.15f", encoding="utf-8")
     original = pd.read_csv("../distribuzioni/empiriche/Strutture/StruttureDistribution.csv", keep_default_na=False,
                            dtype={"CODICE STRUTTURA DI RICOVERO": str}, index_col="CODICE STRUTTURA DI RICOVERO")
-    with open("../statistiche/" + inspect.currentframe().f_code.co_name + "/correlation.txt", "w") as f:
+    with open(f"../statistiche/{name}/correlation.txt", "w") as f:
         for col in ["RICOVERI DS", "RICOVERI DH", "RICOVERI DO"]:
             pearson = concat[col + "_MEDIA"].corr(original[col], method="pearson")
             spearman = concat[col + "_MEDIA"].corr(original[col], method="spearman")
@@ -193,46 +108,42 @@ def test_hospitalization_type_distributions():
             f.write("\n")
 
 
-def test_beds_stats():
-    runs = 2  # FIXME
-    sim_time_days = 30  # FIXME
-    beds_stats = []
+def calc_beds_stats(name: str, runs: int):
+    stats: list[pd.DataFrame] = []
     for i in range(runs):
-        simulation(trace=False, sim_time_days=sim_time_days, animate=False, speed=10, mutations=[],
-                   statistics_dir="../statistiche/" + inspect.currentframe().f_code.co_name + f"/runs/{i}/",
-                   random_seed=i)
-        csv = pd.read_csv("../statistiche/" + inspect.currentframe().f_code.co_name + f"/runs/{i}/stats_beds.csv",
-                          keep_default_na=False, index_col="VARIABILE CALCOLATA").transpose()
-        beds_stats.append(csv)
+        csv = pd.read_csv(f"../statistiche/{name}/runs/{i}/stats_beds.csv", keep_default_na=False,
+                          index_col="VARIABILE CALCOLATA").transpose()
+        stats.append(csv)
     dfs = []
-    for key in beds_stats[0].columns:
-        df = pd.concat((df[key].rename(str(i)).astype(float) for i, df in enumerate(beds_stats)), axis=1)
+    for key in stats[0].columns:
+        df = pd.concat((df[key].rename(str(i)).astype(float) for i, df in enumerate(stats)), axis=1)
         df = df.transpose().mean().rename(key)
         dfs.append(df)
     frame = pd.concat(dfs, axis=1).transpose()
     frame.index.name = "VARIABILE CALCOLATA"
     frame["INTERVALLO CONFIDENZA INFERIORE"] = frame["MEDIA"] - (0.8159 * (np.sqrt(frame["VARIANZA"] / runs)))
     frame["INTERVALLO CONFIDENZA SUPERIORE"] = frame["MEDIA"] + (0.8159 * (np.sqrt(frame["VARIANZA"] / runs)))
-    frame.to_csv("../statistiche/" + inspect.currentframe().f_code.co_name + "/stats_beds_mean.csv",
-                 float_format="%.15f", encoding="utf-8")
+    frame.to_csv(f"../statistiche/{name}/stats_beds_mean.csv", float_format="%.15f", encoding="utf-8")
+
+
+def base_test(runs: int, sim_time_days: int, name: str, mutations=None):
+    if mutations is None:
+        mutations = []
+    for i in range(runs):
+        simulation(trace=False, sim_time_days=sim_time_days, animate=False, speed=10, mutations=mutations,
+                   statistics_dir=f"../statistiche/{name}/runs/{i}/", random_seed=i)
+    calc_beds_stats(name, runs)
+    calc_hospitalization_type_distributions(name, runs)
+    calc_mdc_distributions(name, runs)
 
 
 if __name__ == "__main__":
     with CodeTimer():
-        # test_main()
-        # test_mutations_example()
-        # test_delete_5_biggest_structures()
-        # test_delete_5_smallest_structures()
-        # test_decrease_all_beds_10()
-        # test_increase_all_beds_10()
-        # test_decrease_all_beds_5()
-        # test_increase_all_beds_5()
-        # test_change_convalescence_avg_time()
-        # test_mdc_distributions()
-        test_hospitalization_type_distributions()
-        # test_beds_stats()
-        # p1 = multiprocessing.Process(name='delete_5_biggest', target=test_delete_5_smallest_structures())
-        # p2 = multiprocessing.Process(name='delete_5_smallest', target=test_delete_5_smallest_structures())
-        # p1.start()
-        # p2.start()
-        pass
+        base_test(runs=10, sim_time_days=1, name="base_test")
+        # base_test(runs=10, sim_time_days=1, mutations=test_mutations_example(), name="test_mutations_example")
+        # base_test(runs=10, sim_time_days=1, mutations=test_increase_all_beds_percent(5), name="test_increase_all_beds_5_percent")
+        # base_test(runs=10, sim_time_days=1, mutations=test_increase_all_beds_percent(10), name="test_increase_all_beds_10_percent")
+        # base_test(runs=10, sim_time_days=1, mutations=test_decrease_all_beds_percent(5), name="test_decrease_all_beds_5_percent")
+        # base_test(runs=10, sim_time_days=1, mutations=test_decrease_all_beds_percent(10), name="test_decrease_all_beds_10_percent")
+        # base_test(runs=10, sim_time_days=1, mutations=test_delete_5_smallest_structures(), name="test_delete_5_smallest_structures")#
+        # base_test(runs=10, sim_time_days=1, mutations=test_delete_5_biggest_structures(), name="test_delete_5_biggest_structures")
