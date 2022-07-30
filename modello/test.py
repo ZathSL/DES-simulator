ed578bdf15ada865f92e4258b3abeee3617fce9b
Alessandro Contenti <a.contenti@gmail.com>
2022-07-30 17:50:02 +0200

    - updated test to make tests divisible
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- modello/test.py
+++ modello/test.py
@@ -5,13 +5,18 @@ from stats import calc_mdc_distribution_stats, calc_hospitalization_type_stats, 
 from util import CodeTimer
 
 
-def test(runs: int, duration: int, name: str, mutations=None):
+def test(runs: range, duration: int, name: str, mutations=None, stats=True):
     if mutations is None:
         mutations = []
     shutil.rmtree(f"../statistiche/{name}", ignore_errors=True)
-    for i in range(runs):
+    for i in runs:
         simulation(trace=False, sim_time_days=duration, animate=False, speed=10, mutations=mutations,
                    statistics_dir=f"../statistiche/{name}/runs/{i}/", random_seed=i)
+    if stats:
+        calc_stats(name, len(runs))
+
+
+def calc_stats(name: str, runs: int):
     calc_beds_stats(name, runs)
     calc_hospitalization_type_stats(name, runs)
     calc_mdc_distribution_stats(name, runs)
@@ -19,7 +24,10 @@ def test(runs: int, duration: int, name: str, mutations=None):
 
 if __name__ == "__main__":
     with CodeTimer():
-        test(runs=2, duration=1, name="base")
+        test(runs=range(2), duration=1, name="base")  # test veloce
+        test(runs=range(0, 10), duration=365, name="base", stats=False)
+        test(runs=range(10, 20), duration=365, name="base", stats=False)
+        calc_stats("base", 20)
         # base_test(runs=2, duration=1, mutations=increase_all_beds_percent(5), name="increase_all_beds_5_percent")
         # base_test(runs=2, duration=1, mutations=increase_all_beds_percent(10), name="increase_all_beds_10_percent")
         # base_test(runs=2, duration=1, mutations=decrease_all_beds_percent(5), name="decrease_all_beds_5_percent")
