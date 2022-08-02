b90ef2f4e7a5b380e1c6d283c51496b4e857d81d
Alessandro Contenti <a.contenti@gmail.com>
2022-08-02 16:44:27 +0200

    - updated validation - updated decrease_all_beds_percent_5 stats
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- convalida/validate-hospitalizations.py
+++ convalida/validate-hospitalizations.py
@@ -3,18 +3,18 @@ import pandas as pd
 
 
 def validate_hospitalizations():
-    original = pd.read_csv("../Dati per la convalida/Media dei ricoveri per struttura.csv", keep_default_na=False,
+    original = pd.read_csv("../Dati per la convalida/mean_hospitalization_dataset.csv", keep_default_na=False,
                            dtype={"Codice Struttura Di Ricovero": str})
-    original.set_index("Codice Struttura Di Ricovero", inplace=True)
-    original.index.name = "STRUTTURA"
+    original.set_index("STRUTTURA", inplace=True)
+    original.columns = original.columns.str.split("_", expand=True)
     generated = pd.read_csv("../statistiche/base/hospitalization_type_patients_treated_mean.csv", keep_default_na=False,
                             dtype={"STRUTTURA": str})
     generated.set_index("STRUTTURA", inplace=True)
     generated.columns = generated.columns.str.split("_", expand=True)
-    diff_do = np.absolute(original["Media Ricoveri DO"] - generated["RICOVERI DO", "MEDIA"]).rename("RICOVERI DO")
-    diff_dh = np.absolute(original["Media Ricoveri DH"] - generated["RICOVERI DH", "MEDIA"]).rename("RICOVERI DH")
-    diff_ds = np.absolute(original["Media Ricoveri DS"] - generated["RICOVERI DS", "MEDIA"]).rename("RICOVERI DS")
-    diff_tot = np.absolute(original["Media Totale Ricoveri"] - generated["TOTALE RICOVERI", "MEDIA"]).rename(
+    diff_do = np.absolute(original["RICOVERI DO", "MEDIA"] - generated["RICOVERI DO", "MEDIA"]).rename("RICOVERI DO")
+    diff_dh = np.absolute(original["RICOVERI DH", "MEDIA"] - generated["RICOVERI DH", "MEDIA"]).rename("RICOVERI DH")
+    diff_ds = np.absolute(original["RICOVERI DS", "MEDIA"] - generated["RICOVERI DS", "MEDIA"]).rename("RICOVERI DS")
+    diff_tot = np.absolute(original["TOTALE RICOVERI", "MEDIA"] - generated["TOTALE RICOVERI", "MEDIA"]).rename(
         "TOTALE RICOVERI")
     concat = pd.concat([diff_do, diff_dh, diff_ds, diff_tot], axis=1)
     concat = pd.concat([concat, pd.DataFrame.from_dict({
