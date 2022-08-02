57f5fb5b3b635a34791bf8831c5701b1eb7fc34e
Alessandro Contenti <a.contenti@gmail.com>
2022-08-02 17:10:55 +0200

    - bug fix in validation
    Signed-off-by: Alessandro Contenti <a.contenti@gmail.com>

--- convalida/validate-hospitalizations.py
+++ convalida/validate-hospitalizations.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 
@@ -6,15 +5,17 @@ def validate_hospitalizations():
     original = pd.read_csv("../Dati per la convalida/mean_hospitalization_dataset.csv", keep_default_na=False,
                            dtype={"Codice Struttura Di Ricovero": str})
     original.set_index("STRUTTURA", inplace=True)
+    original.sort_index(inplace=True)
     original.columns = original.columns.str.split("_", expand=True)
     generated = pd.read_csv("../statistiche/base/hospitalization_type_patients_treated_mean.csv", keep_default_na=False,
                             dtype={"STRUTTURA": str})
     generated.set_index("STRUTTURA", inplace=True)
+    generated.sort_index(inplace=True)
     generated.columns = generated.columns.str.split("_", expand=True)
-    diff_do = np.absolute(original["RICOVERI DO", "MEDIA"] - generated["RICOVERI DO", "MEDIA"]).rename("RICOVERI DO")
-    diff_dh = np.absolute(original["RICOVERI DH", "MEDIA"] - generated["RICOVERI DH", "MEDIA"]).rename("RICOVERI DH")
-    diff_ds = np.absolute(original["RICOVERI DS", "MEDIA"] - generated["RICOVERI DS", "MEDIA"]).rename("RICOVERI DS")
-    diff_tot = np.absolute(original["TOTALE RICOVERI", "MEDIA"] - generated["TOTALE RICOVERI", "MEDIA"]).rename(
+    diff_do = original["RICOVERI DO", "MEDIA"].sub(generated["RICOVERI DO", "MEDIA"]).abs().rename("RICOVERI DO")
+    diff_dh = original["RICOVERI DH", "MEDIA"].sub(generated["RICOVERI DH", "MEDIA"]).abs().rename("RICOVERI DH")
+    diff_ds = original["RICOVERI DS", "MEDIA"].sub(generated["RICOVERI DS", "MEDIA"]).abs().rename("RICOVERI DS")
+    diff_tot = original["TOTALE RICOVERI", "MEDIA"].sub(generated["TOTALE RICOVERI", "MEDIA"]).abs().rename(
         "TOTALE RICOVERI")
     concat = pd.concat([diff_do, diff_dh, diff_ds, diff_tot], axis=1)
     concat = pd.concat([concat, pd.DataFrame.from_dict({
